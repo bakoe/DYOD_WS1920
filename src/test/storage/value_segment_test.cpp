e1f523de860ed0b42d671efd16843d252373cf34
bakoe <ba.koe96@gmail.com>
2019-11-12 15:16:02 +0100

    Enable ValueSegment memory_usage test, add implementation

--- src/test/storage/value_segment_test.cpp
+++ src/test/storage/value_segment_test.cpp
@@ -47,11 +47,21 @@ TEST_F(StorageValueSegmentTest, AddValueOfDifferentType) {
   EXPECT_THROW(double_value_segment.append("Hi"), std::exception);
 }
 
-// TEST_F(StorageValueSegmentTest, MemoryUsage) {
-//   int_value_segment.append(1);
-//   EXPECT_EQ(int_value_segment.estimate_memory_usage(), size_t{4});
-//   int_value_segment.append(2);
-//   EXPECT_EQ(int_value_segment.estimate_memory_usage(), size_t{8});
-// }
+TEST_F(StorageValueSegmentTest, MemoryUsage) {
+  int_value_segment.append(1);
+  EXPECT_EQ(int_value_segment.estimate_memory_usage(), size_t{4});
+  int_value_segment.append(2);
+  EXPECT_EQ(int_value_segment.estimate_memory_usage(), size_t{8});
+
+  string_value_segment.append("Hello");
+  EXPECT_EQ(string_value_segment.estimate_memory_usage(), sizeof(std::string));
+  string_value_segment.append("World");
+  EXPECT_EQ(string_value_segment.estimate_memory_usage(), 2 * sizeof(std::string));
+
+  double_value_segment.append(3.14);
+  EXPECT_EQ(double_value_segment.estimate_memory_usage(), sizeof(double));
+  double_value_segment.append(42.0);
+  EXPECT_EQ(double_value_segment.estimate_memory_usage(), 2 * sizeof(double));
+}
 
 }  // namespace opossum
